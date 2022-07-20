6954d5fd659143e3065ff4f10af4003d7393301d
Claus Rørbech <claus.rorbech@mongodb.com>
2022-07-20 11:36:44 +0200

    Class based definition

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,9 +2,20 @@ import org.jetbrains.kotlinx.dataframe.DataColumn
 import org.jetbrains.kotlinx.dataframe.api.columnOf
 import org.jetbrains.kotlinx.dataframe.api.toColumn
 
+interface Person {
+    val name: String
+    val age: Int
+}
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
     val student: DataColumn<String> by columnOf("Alice", "Bob")
     println(listOf("Alice", "Bob").toColumn("name"))
     println(student)
+    val df = dataFrameOf("name", "age")(
+        "Alice", 15,
+        "Bob", 20
+    ).cast<Person>()
+    println(df)
+}
